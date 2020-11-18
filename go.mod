5a3667ba91dce2a2e23b5de6b3a900c5c0d7d497
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-11-18 16:25:41 -0600

    add tests for gnmatcher #20

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0
+	github.com/stretchr/testify v1.5.1
 	gitlab.com/gogna/gnparser v0.14.1
 	golang.org/x/sys v0.0.0-20201101102859-da207088b7d1 // indirect
 )
---
+++ rest/bug_test.go
@@ -0,0 +1,67 @@
+package rest
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	vlib "github.com/gnames/gnlib/domain/entity/verifier"
+	"github.com/gnames/gnlib/encode"
+	"github.com/stretchr/testify/assert"
+)
+
+const url = "http://:8888/"
+
+var bugs = []struct {
+	name           string
+	matchType      vlib.MatchType
+	matchCanonical string
+	desc           string
+}{
+	{
+		name:           "Tillaudsia utriculata",
+		matchType:      vlib.Fuzzy,
+		matchCanonical: "Tillandsia utriculata",
+		desc:           "Misspelling of Tillandsia",
+	},
+	{
+		name:           "Drosohila melanogaster",
+		matchType:      vlib.Fuzzy,
+		matchCanonical: "Drosophila melanogaster",
+		desc:           "Misspelling of Drosophila",
+	},
+	{
+		name:           "Acacia nur",
+		matchType:      vlib.PartialExact,
+		matchCanonical: "Acacia",
+		desc:           "Should not match 'Acacia dura', ep. is too short",
+	},
+}
+
+func TestBugs(t *testing.T) {
+	req, err := encode.GNjson{}.Encode(params())
+	assert.Nil(t, err)
+	r := bytes.NewReader(req)
+	resp, err := http.Post(url+"verification", "application/x-binary", r)
+	assert.Nil(t, err)
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+
+	var verif []vlib.Verification
+	err = encode.GNjson{}.Decode(respBytes, &verif)
+	assert.Nil(t, err)
+
+	for i, v := range bugs {
+		assert.Equal(t, verif[i].BestResult.MatchedCanonicalSimple, v.matchCanonical)
+		assert.Equal(t, verif[i].MatchType, v.matchType)
+	}
+}
+
+func params() vlib.VerifyParams {
+	ns := make([]string, len(bugs))
+	for i, v := range bugs {
+		ns[i] = v.name
+	}
+	return vlib.VerifyParams{NameStrings: ns}
+}
